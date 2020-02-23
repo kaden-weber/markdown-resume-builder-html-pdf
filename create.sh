9f657bca07d168e6823dd68969d8eecbb1c06ae9
Kaden Weber <kweber142@gmail.com>
2020-02-22 22:36:31 -0700

    jekyll html cleanup, remove pdf warning

--- create.sh
+++ create.sh
@@ -1,3 +1,4 @@
-pandoc sections/*.md -o result.pdf --pdf-engine=weasyprint --metadata title="..."
+pandoc sections/*.md -o result.pdf --pdf-engine=weasyprint --metadata title="" --quiet
 pandoc sections/*.md -o index.html
-echo -e "---\n---\n$(cat index.html)" > index.html
+echo -e "---\nlayout: default\n---\n<body>\n$(cat index.html)" > index.html
+echo "</body>" >> index.html
