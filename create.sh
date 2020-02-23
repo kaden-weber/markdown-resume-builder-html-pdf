eb31322a8e86c2d3cb276d3444f81e962fa2dad9
Kaden Weber <kweber142@gmail.com>
2020-02-22 22:15:21 -0700

    added front matter for jekyll

--- create.sh
+++ create.sh
@@ -1,2 +1,3 @@
-pandoc *.md -o result.pdf --pdf-engine=weasyprint --metadata title="Kaden Weber"
-pandoc *.md -o index.html
+pandoc sections/*.md -o result.pdf --pdf-engine=weasyprint --metadata title="..."
+pandoc sections/*.md -o index.html
+echo -e "---\n---\n$(cat index.html)" > index.html
