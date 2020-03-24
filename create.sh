86ad5f91d7314fb3eacf75bdd3c6252f132f84af
Kaden Weber <kweber142@gmail.com>
2020-03-24 16:02:37 -0600

    link added for pdf

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
-pandoc sections/*.md -o result.pdf --pdf-engine=weasyprint --metadata title="" --quiet
+pandoc pdf/*.md sections/*.md -o result.pdf --pdf-engine=weasyprint --metadata title="" --quiet
 pandoc sections/*.md -o index.html
 echo -e "---\nlayout: default\n---\n<body>\n$(cat index.html)" > index.html
 echo "</body>" >> index.html
